6bda8b779f2ced47999b7bb22eff1387c1afca98
Robin9800 <robin.costanzo@gmail.com>
2022-03-21 22:23:39 +0100

    "nineTitans" array added

---
+++ script.js
@@ -0,0 +1,40 @@
+/*Aggiungere un file js in cui definire *un array di oggetti* che rappresentano i membri del team.
+Ogni membro ha le informazioni necessarie
+per stampare la relativa card: *Nome, Ruolo e Foto*.
+Una volta definita la struttura dati, prendendo come riferimento la card 
+di esempio presente nell'html, stampare dinamicamente una card per ogni membro del team.*/
+
+
+// array di oggetti
+const nineTitans = [
+    {
+        pic: "img/Attack Titan.jpg",
+        nameMember: "Attack Titan",
+        shifter: "Eren Jeager"
+    },
+    {
+        pic: "img/Armored Titan.jpg",
+        nameMember: "Armored Titan",
+        shifter: "Reiner Braun"
+    },
+    {
+        pic: "img/Beast Titan.jpg",
+        nameMember: "Beast Titan",
+        shifter: "Zeke Jeager"
+    },
+    {
+        pic: "img/Colossal Titan.jpg",
+        nameMember: "Colossal Titan",
+        shifter: "Armin Arlert"
+    },
+    {
+        pic: "img/Female Titan.jpg",
+        nameMember: "Female Titan",
+        shifter: "Annie Leonhart"
+    },
+    {
+        pic: "img/Jaw Titan.jpg",
+        nameMember: "Jaw Titan",
+        shifter: "Porco Galliard"
+    },
+];
