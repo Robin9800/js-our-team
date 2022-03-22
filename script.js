e2494b08a3842184745e00af708569409b235325
Robin9800 <robin.costanzo@gmail.com>
2022-03-22 15:24:49 +0100

    creazioneTitan function content

--- script.js
+++ script.js
@@ -7,32 +7,32 @@ di esempio presente nell'html, stampare dinamicamente una card per ogni membro d
 //Creo un array di oggetti
 const nineTitans = [
     {
-        pic: "img/Attack Titan.jpg",
+        img: "img/Attack Titan.jpg",
         nameMember: "Attack Titan",
         shifter: "Eren Jeager"
     },
     {
-        pic: "img/Armored Titan.jpg",
+        img: "img/Armored Titan.jpg",
         nameMember: "Armored Titan",
         shifter: "Reiner Braun"
     },
     {
-        pic: "img/Beast Titan.jpg",
+        img: "img/Beast Titan.jpg",
         nameMember: "Beast Titan",
         shifter: "Zeke Jeager"
     },
     {
-        pic: "img/Colossal Titan.jpg",
+        img: "img/Colossal Titan.jpg",
         nameMember: "Colossal Titan",
         shifter: "Armin Arlert"
     },
     {
-        pic: "img/Female Titan.jpg",
+        img: "img/Female Titan.jpg",
         nameMember: "Female Titan",
         shifter: "Annie Leonhart"
     },
     {
-        pic: "img/Jaw Titan.jpg",
+        img: "img/Jaw Titan.jpg",
         nameMember: "Jaw Titan",
         shifter: "Porco Galliard"
     },
@@ -55,32 +55,32 @@ function creazioneTitan(titan) {
     const contenitoreImmagine = document.createElement('div');
     // aggiungo una classe all'elemento contenitore immagine 
     contenitoreImmagine.classList.add('card-image');
-    // appendo l'elemento al div team card 
-    contenitoreCarte.appendChild(contenitoreImmagine);
+    // AGGIUNGO LA CLASSE CONTENITORE IMG DENTRO IL CONTENITORE GENERALE
+    contenitoreImmagine.appendChild(contenitoreCarte);
     // creo elemento immagine 
-    const elementoImmagine = document.createElement('img');
+    let image = document.createElement('img');
     // Nel div carta appendo l'elemento immagine 
-    contenitoreImmagine.appendChild(elementoImmagine);
+    contenitoreCarte.appendChild(image);
     // all'elemento immagine vado a inserire l'img dell'oggetto
-    elementoImmagine.src = titan.immagine;
+    image.src = titan.img;
     // Creo il div che conterrà tag h3 e tag p 
-    const contenitoreTesto = document.createElement('div');
+    const containerText = document.createElement('div');
     // all'elemento div aggiungo una classe
-    contenitoreTesto.classList.add('card-text');
+    containerText.classList.add('card-text');
     // al contenitore Card appendo l'elemento div testo creato 
-    contenitoreCarte.appendChild(contenitoreTesto);
+    containerCard.appendChild(containerText);
     // creo un nuovo elemento che sarà il nome della carta 
-    const nomePersonaggio = document.createElement('h3');
+
     // appendo l'elemendo creato al contenitore testo 
-    contenitoreTesto.appendChild(nomePersonaggio);
+ 
     // all'elemento nomePersonaggio vado ad inserire il nome dell'oggetto 
-    nomePersonaggio.innerText = titan.nome;
+
     // creo un nuovo elemento che sarà il ruolo della carta 
-    const ruoloPersonaggio = document.createElement('p');
+
     // appendo l'elemendo creato al contenitore testo
-    contenitoreTesto.appendChild(ruoloPersonaggio);
+  
     // all'elemento ruoloPersonaggio vado ad inserire il ruolo dell'oggetto 
-    titanShifter.innerText = titan.shifter;
+
 
 }
 
